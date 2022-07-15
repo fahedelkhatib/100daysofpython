a137fe45ad083a9cad424db793c9792edc4269c6
fahedelkhatib <felkh002@ucr.edu>
2022-07-15 15:54:11 -0700

    day 5 easy mode complete

--- 5_Password_Generator.py
+++ 5_Password_Generator.py
@@ -13,6 +13,16 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+password = ""
+for letter in range(0, nr_letters):
+    password = password + letters[random.randint(0, len(letters) - 1)]
+for symbol in range(0, nr_symbols):
+    password = password + symbols[random.randint(0, len(symbols) - 1)]
+for number in range(0, nr_numbers):
+    password = password + numbers[random.randint(0, len(numbers) - 1)]
+
+print(password)
+
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
