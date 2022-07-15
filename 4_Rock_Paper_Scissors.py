6742203cb3e5312463fca9e730d3ff62bf830da5
fahedelkhatib <felkh002@ucr.edu>
2022-07-15 15:11:25 -0700

    took me 5 days to realize i needed this lmao

--- 4_Rock_Paper_Scissors.py
+++ 4_Rock_Paper_Scissors.py
@@ -30,13 +30,19 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+
+#create array with ascii graphics
 graphics = [rock, paper, scissors]
+
+#get user and computer choices
 userChoice = int(input("What do you choose? Type 0 for rock, 1 for paper, or 2 for scissors\n"))
 compChoice = random.randint(0,2)
 
+#print choices using some f-strings for brevity
 print(f"Your choice:\n{graphics[userChoice]}")
 print(f"Computer choice:\n{graphics[compChoice]}")
 
+#decide the winner and print result
 if(userChoice == 0):
     if(compChoice == 0):
         print("Tie game.")
