256d7f44746ec98c0edafde3203092368928acea
fahedelkhatib <fahedelkhatib12@gmail.com>
2022-07-15 22:15:40 -0700

    imperfect solution to day 5 project

--- 5_Password_Generator.py
+++ 5_Password_Generator.py
@@ -12,17 +12,39 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
 password = ""
-for letter in range(0, nr_letters):
-    password = password + letters[random.randint(0, len(letters) - 1)]
-for symbol in range(0, nr_symbols):
-    password = password + symbols[random.randint(0, len(symbols) - 1)]
-for number in range(0, nr_numbers):
-    password = password + numbers[random.randint(0, len(numbers) - 1)]
+diffChoice = input("would you like easy mode or hard mode??\n")
 
-print(password)
+if(diffChoice == "yes"):
 
+    password = ""
+    for letter in range(0, nr_letters):
+        password = password + letters[random.randint(0, len(letters) - 1)]
+    for symbol in range(0, nr_symbols):
+        password = password + symbols[random.randint(0, len(symbols) - 1)]
+    for number in range(0, nr_numbers):
+        password = password + numbers[random.randint(0, len(numbers) - 1)]
+
+    
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+#this solution is not perfect, as it does not actually use the distribution of character types specified
+else:
+    lengthOfPass = nr_letters + nr_symbols + nr_numbers
+    numberOfTypesAvailable = 3
+
+    for s in range(0, lengthOfPass):
+        characterChoice = random.randint(0, 3)
+        if(characterChoice == 0 and nr_letters > 0):
+            password = password + letters[random.randint(0, len(letters) - 1)]
+            nr_letters -= 1
+        elif(characterChoice == 1 and nr_symbols != 0):
+            password = password + symbols[random.randint(0, len(symbols) - 1)]
+            nr_symbols -= 1
+        elif(characterChoice == 2 and nr_numbers != 0):
+            password = password + numbers[random.randint(0, len(numbers) - 1)]
+            nr_numbers -= 1
+        #choose which type of character you want randomly
+        
+print(f"Password generated: {password}")
